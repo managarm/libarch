2dfacdbc882f3efe08dcddfd28eb781651da6031
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-04-22 21:10:55 +0200

    Add a "mask and set" field operation

--- include/arch/bits.hpp
+++ include/arch/bits.hpp
@@ -16,6 +16,29 @@ private:
 	B _bits;
 };
 
+// represents a fixed size vector of bits with a value mask.
+template<typename B>
+struct masked_bit_value {
+	explicit masked_bit_value(B bits, B mask)
+	: _bits(bits), _mask(mask) { }
+
+	explicit operator B () {
+		return _bits;
+	}
+
+	B mask() const {
+		return _mask;
+	}
+
+	B bits() const {
+		return _bits;
+	}
+
+private:
+	B _bits;
+	B _mask;
+};
+
 // represents a fixed size vector of bits.
 template<typename B>
 struct bit_value {
@@ -35,6 +58,17 @@ struct bit_value {
 		return *this;
 	}
 
+	// allow building a value from multiple bit vectors.
+	bit_value operator| (masked_bit_value<B> other) const {
+		return bit_value(_bits | other.bits());
+	}
+	bit_value &operator|= (masked_bit_value<B> other) {
+		*this = *this | other;
+		return *this;
+	}
+
+
+
 	// allow masking out individual bits.
 	bit_value operator& (bit_mask<B> other) const {
 		return bit_value(_bits & static_cast<B>(other));
@@ -43,6 +77,16 @@ struct bit_value {
 		*this = *this & other;
 		return *this;
 	}
+
+	// combined masking out and building values
+	bit_value operator/ (masked_bit_value<B> other) const {
+		return bit_value((_bits & ~other.mask()) | other.bits());
+	}
+	bit_value &operator/= (masked_bit_value<B> other) {
+		*this = *this * other;
+		return *this;
+	}
+
 
 private:
 	B _bits;
@@ -58,9 +102,8 @@ struct field {
 	explicit constexpr field(int shift, int num_bits)
 	: _shift(shift), _mask((B(1) << num_bits) - 1) { }
 
-	// allow construction of bit vectors from fields.
-	bit_value<B> operator() (T value) const {
-		return bit_value<B>((static_cast<B>(value) & _mask) << _shift);
+	masked_bit_value<B> operator() (T value) const {
+		return masked_bit_value<B>((static_cast<B>(value) & _mask) << _shift, _mask << _shift);
 	}
 
 	// allow inversion of this field to a bit mask.
