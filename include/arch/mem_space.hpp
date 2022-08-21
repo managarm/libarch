ead0f394365ad4c5436f73b93a1a08ea117fee53
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2022-08-21 21:09:29 +0200

    mem_space: Partially port to RISC-V 64
    This port is complete except for atomic_exchange().

--- include/arch/mem_space.hpp
+++ include/arch/mem_space.hpp
@@ -1,11 +1,11 @@
-
 #if defined(__i386__) || defined(__x86_64__)
 #	include <arch/x86/mem_space.hpp>
 #elif defined(__aarch64__)
 #	include <arch/aarch64/mem_space.hpp>
 #elif defined(__arm__)
 #	include <arch/arm/mem_space.hpp>
+#elif defined(__riscv) && (__riscv_xlen == 64)
+#	include <arch/riscv64/mem_space.hpp>
 #else
 #	error Unsupported architecture
 #endif
-
